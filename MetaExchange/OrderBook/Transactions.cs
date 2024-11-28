0467f579a62a5b777ebeeaaacf88b593b49e17f6
Matjaz <vmatkov@gmail.com>
2024-11-28 17:51:09 +0100

    [MetaExchange] Buying BTC part of Method Finished. Implemented 4 test for buying BTC.

--- MetaExchange/OrderBook/Transactions.cs
+++ MetaExchange/OrderBook/Transactions.cs
@@ -19,6 +19,10 @@ namespace MetaExchange.OrderBook
         }
 
         private void GenerateCryptoExchanges(string filePath) {
+            
+            OrderBooks.Clear();
+            CryptoExchanges.Clear();
+            
             var orderBooks  = File.ReadAllLines(filePath);
 
             foreach (var orderBook in orderBooks) 
@@ -34,14 +38,132 @@ namespace MetaExchange.OrderBook
                 if(temp != null) OrderBooks.Add(temp);
             }
 
-            for (var i = 0; i < OrderBooks.Count; i++)
+            if(OrderBooks != null)
             {
-                var orderBook = OrderBooks[i];
-                orderBook.Asks = orderBook.Asks.OrderBy(o => o.Order?.Price).ToList();
-                orderBook.Bids = orderBook.Bids.OrderByDescending(o => o.Order?.Price).ToList();
+                for (var i = 0; i < OrderBooks.Count; i++)
+                {
+                    var orderBook = OrderBooks[i];
+                    orderBook.Asks = orderBook.Asks.OrderBy(o => o.Order?.Price).ToList();
+                    orderBook.Bids = orderBook.Bids.OrderByDescending(o => o.Order?.Price).ToList();
+                }
+
+                for (var i = 0; i < OrderBooks.Count; i++) CryptoExchanges.Add(new() { OrderBook = OrderBooks[i] });
+            }
+        }
+
+        public List<(string ExchangeName, double Price, double Amount)> GetBestTrades(string type, double amount)
+        {
+            var result = new List<(string ExchangeName, double Price, double Amount)>();
+            var remainingAmount = amount;
+
+            if(type.ToUpper() == "BUY")
+            {
+                if(CryptoExchanges != null)
+                {
+                    var allAsks = CryptoExchanges.SelectMany(exchange => exchange.OrderBook.Asks.Select(ask => new
+                    {
+                        Id                  = ask.Order.Id,
+                        ExchangeName        = exchange.OrderBook.AcqTime.ToString(),
+                        Price               = ask.Order.Price,
+                        AvailableBtc        = ask.Order.Amount,
+                        Exchange            = exchange
+                    }))
+                    .OrderBy(ask => (ask.Price/ask.AvailableBtc)) // order by price per 1 BTC
+                    .ToList();
+
+                    for (var i = 0;i < allAsks.Count; i++)
+                    {
+                        if (remainingAmount <= 0) break;
+                        double buyingAmount = Math.Min(remainingAmount, allAsks[i].AvailableBtc);
+
+                        if(buyingAmount > 0)
+                        {
+                            var affordable = allAsks[i].Exchange.EurBalance / remainingAmount > allAsks[i].Price / allAsks[i].AvailableBtc;
+                            if (affordable)
+                            {
+                                result.Add((allAsks[i].ExchangeName, (allAsks[i].Price / allAsks[i].AvailableBtc) * buyingAmount, buyingAmount));
+                                allAsks[i].Exchange.EurBalance -= (allAsks[i].Price / allAsks[i].AvailableBtc) * buyingAmount;
+                                remainingAmount -= buyingAmount;
+                            }
+                            else continue;
+
+                        }
+                    }
+
+                    if(remainingAmount > 0)
+                    {
+                        Console.WriteLine($"You could only purchase {amount - remainingAmount} in the currect constraint.");
+                    }
+                }
+            }
+            else if (type.ToUpper() == "SELL")
+            {
+
             }
 
-            for(var i = 0; i < OrderBooks.Count; i++) CryptoExchanges.Add(new() { OrderBook = OrderBooks[i] });
+            return result;
+        }
+
+        public List<(string ExchangeName, double Price, double Amount)> GetBestTrades(string type, double amount, List<(double, double)> predefinedBalances)
+        {
+            var result = new List<(string ExchangeName, double Price, double Amount)>();
+            var remainingAmount = amount;
+
+            if (type.ToUpper() == "BUY")
+            {
+                if (CryptoExchanges != null)
+                {
+                    // FOR TEST PURPUSES
+                    if (predefinedBalances != null)
+                    {
+                        for (int i = 0; i < predefinedBalances.Count; i++)
+                        {
+                            CryptoExchanges[i].EurBalance = predefinedBalances[i].Item1;
+                        }
+                    }
+
+                    var allAsks = CryptoExchanges.SelectMany(exchange => exchange.OrderBook.Asks.Select(ask => new
+                    {
+                        Id = ask.Order.Id,
+                        ExchangeName = exchange.OrderBook.AcqTime.ToString(),
+                        Price = ask.Order.Price,
+                        AvailableBtc = ask.Order.Amount,
+                        Exchange = exchange
+                    }))
+                    .OrderBy(ask => (ask.Price / ask.AvailableBtc)) // order by price per 1 BTC
+                    .ToList();
+
+                    for (var i = 0; i < allAsks.Count; i++)
+                    {
+                        if (remainingAmount <= 0) break;
+                        double buyingAmount = Math.Min(remainingAmount, allAsks[i].AvailableBtc);
+
+                        if (buyingAmount > 0)
+                        {
+                            var affordable = allAsks[i].Exchange.EurBalance / remainingAmount > allAsks[i].Price / allAsks[i].AvailableBtc;
+                            if (affordable)
+                            {
+                                result.Add((allAsks[i].ExchangeName, (allAsks[i].Price / allAsks[i].AvailableBtc) * buyingAmount, buyingAmount));
+                                allAsks[i].Exchange.EurBalance -= (allAsks[i].Price / allAsks[i].AvailableBtc) * buyingAmount;
+                                remainingAmount -= buyingAmount;
+                            }
+                            else continue;
+
+                        }
+                    }
+
+                    if (remainingAmount > 0)
+                    {
+                        Console.WriteLine($"You could only purchase {amount - remainingAmount} in the currect constraint.");
+                    }
+                }
+            }
+            else if (type.ToUpper() == "SELL")
+            {
+
+            }
+
+            return result;
         }
     }
 }
--- MetaExchange/Program.cs
+++ MetaExchange/Program.cs
@@ -1,7 +1,96 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MetaExchange.OrderBook;
 
-Console.WriteLine("Hello, World!");
+#region TEST 1
 
-string inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input", "order_books_data.json");
+Console.WriteLine("--- START TEST 1 ---");
+// Get input file
+string inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input", "test1.json");
+// Init CryptoExchanges and OrderBooks
 Transactions transactions = new(inputFilePath);
+// Init test EUR Balances
+List<(double, double)> predefinedBalances = new() { (190, 0), (112.5, 0), (249, 0) };
+// call function
+var bestTrades = transactions.GetBestTrades("buy", 4.20, predefinedBalances);
+// write out trades
+if (bestTrades.Count > 0)
+{
+    for(int i = 0; i < bestTrades.Count; i++)
+    {
+        Console.WriteLine(string.Concat("Exchange Timestamp: ", bestTrades[i].ExchangeName, " Amount bought: ", bestTrades[i].Amount, " for ", bestTrades[i].Price, " EUR."));
+    }
+}
+Console.WriteLine("--- END TEST 1 ---");
+
+#endregion
+
+#region TEST 2
+
+Console.WriteLine("--- START TEST 2 ---");
+// Get input file
+inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input", "test1.json");
+// Init CryptoExchanges and OrderBooks
+transactions = new(inputFilePath);
+// Init test EUR Balances
+predefinedBalances = new() { (190, 0), (650, 0), (249, 0) };
+// call function
+bestTrades = transactions.GetBestTrades("buy", 4.20, predefinedBalances);
+// write out trades
+if (bestTrades.Count > 0)
+{
+    for (int i = 0; i < bestTrades.Count; i++)
+    {
+        Console.WriteLine(string.Concat("Exchange Timestamp: ", bestTrades[i].ExchangeName, " Amount bought: ", bestTrades[i].Amount, " for ", bestTrades[i].Price, " EUR."));
+    }
+}
+Console.WriteLine("--- END TEST 2 ---");
+
+#endregion
+
+#region TEST 3
+
+Console.WriteLine("--- START TEST 3---");
+// Get input file
+inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input", "test2.txt");
+// Init CryptoExchanges and OrderBooks
+transactions = new(inputFilePath);
+// Init test EUR Balances
+predefinedBalances = new() { (190, 0), (650, 0), (249, 0) };
+// call function
+bestTrades = transactions.GetBestTrades("buy", 4.20, predefinedBalances);
+// write out trades
+if (bestTrades.Count > 0)
+{
+    for (int i = 0; i < bestTrades.Count; i++)
+    {
+        Console.WriteLine(string.Concat("Exchange Timestamp: ", bestTrades[i].ExchangeName, " Amount bought: ", bestTrades[i].Amount, " for ", bestTrades[i].Price, " EUR."));
+    }
+}
+Console.WriteLine("--- END TEST 3 ---");
+
+#endregion
+
+#region TEST 4
+
+Console.WriteLine("--- START TEST 4 ---");
+// Get input file
+inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input", "test2.txt");
+// Init CryptoExchanges and OrderBooks
+transactions = new(inputFilePath);
+// Init test EUR Balances
+predefinedBalances = new() { (100000, 0), (100000, 0), (100000, 0) };
+// call function
+bestTrades = transactions.GetBestTrades("buy", 20, predefinedBalances);
+// write out trades
+if (bestTrades.Count > 0)
+{
+    for (int i = 0; i < bestTrades.Count; i++)
+    {
+        Console.WriteLine(string.Concat("Exchange Timestamp: ", bestTrades[i].ExchangeName, " Amount bought: ", bestTrades[i].Amount, " for ", bestTrades[i].Price, " EUR."));
+    }
+}
+Console.WriteLine("--- END TEST 4 ---");
+
+#endregion
+
+Console.ReadKey();
