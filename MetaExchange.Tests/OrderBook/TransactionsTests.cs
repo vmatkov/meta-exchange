06e4f235d1326e5d9beda87ee9a5b0ebe1291df2
Matjaz <vmatkov@gmail.com>
2024-12-03 15:44:11 +0100

    [MetaExchangeTests] Added selling tests.

--- MetaExchange.Tests/OrderBook/TransactionsTests.cs
+++ MetaExchange.Tests/OrderBook/TransactionsTests.cs
@@ -7,6 +7,7 @@ namespace MetaExchange.Tests.OrderBook
 {
     public class TransactionsTests
     {
+        #region Buying
         [Fact]
         public void Transactions_GetBestTrades_BuyAllBTC()
         {
@@ -66,5 +67,66 @@ namespace MetaExchange.Tests.OrderBook
             //Assert
             result.Should().Equal(expectedResult);
         }
+        #endregion Buying
+        #region Selling
+        [Fact]
+        public void Transactions_GetBestTrades_SellAllBTC()
+        {
+            //Arrange - variables, classes, mocks
+            string inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input", "test1.json");
+            Transactions transactions = new(inputFilePath);
+            List<(decimal, decimal)> predefinedBalances = new() { (868.05m, 2.50m), (15384m, 3m), (253m, 11m) };
+
+            //Act
+            var result = transactions.GetBestTrades(Type.Sell, 16.50m, predefinedBalances);
+            var expectedResult = new List<(string, decimal, decimal)>
+            {
+                ("2024-11-27T15:22:00.2518856Z", 8000m, 5m),
+                ("2024-11-27T15:22:00.2518855Z", 3000m, 2m),
+                ("2024-11-27T15:22:00.2518854Z", 2100.0m, 1.5m),
+                ("2024-11-27T15:22:00.2518854Z", 1200m, 1m),
+                ("2024-11-27T15:22:00.2518856Z", 6480m, 6m),
+                ("2024-11-27T15:22:00.2518855Z", 1000m, 1m)
+            };
+
+            //Assert
+            result.Should().NotBeEmpty();
+            result.Should().Equal(expectedResult);
+        }
+
+        [Fact]
+        public void Transactions_GetBestTrades_SellNoBTC()
+        {
+            //Arrange - variables, classes, mocks
+            string inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input", "test1.json");
+            Transactions transactions = new(inputFilePath);
+            List<(decimal, decimal)> predefinedBalances = new() { (868.05m, 0m), (15384m, 0m), (253m, 0m) };
+
+            //Act
+            var result = transactions.GetBestTrades(Type.Sell, 16.50m, predefinedBalances);
+
+            //Assert
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Transactions_GetBestTrades_SellPartOfBTC()
+        {
+            //Arrange - variables, classes, mocks
+            string inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input", "test1.json");
+            Transactions transactions = new(inputFilePath);
+            List<(decimal, decimal)> predefinedBalances = new() { (0, 0), (0, 0), (0, 0.5m) };
+
+            //Act
+            var result = transactions.GetBestTrades(Type.Sell, 0.50m, predefinedBalances);
+            var expectedResult = new List<(string, decimal, decimal)>
+            {
+                ("2024-11-27T15:22:00.2518856Z", 800m, 0.5m)
+            };
+
+            //Assert
+            result.Should().Equal(expectedResult);
+        }
+        #endregion Selling
     }
 }
