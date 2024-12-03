f2ab2c69045dec19c7097427f95c0c446c14f158
Matjaz <vmatkov@gmail.com>
2024-12-03 15:29:18 +0100

    [MetaExchangeTests] Added aditional Buy tests.

--- MetaExchange.Tests/OrderBook/TransactionsTests.cs
+++ MetaExchange.Tests/OrderBook/TransactionsTests.cs
@@ -31,5 +31,40 @@ namespace MetaExchange.Tests.OrderBook
             result.Should().NotBeEmpty();
             result.Should().Equal(expectedResult);
         }
+
+        [Fact]
+        public void Transactions_GetBestTrades_CannotBuyBTC()
+        {
+            //Arrange - variables, classes, mocks
+            string inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input", "test1.json");
+            Transactions transactions = new(inputFilePath);
+            List<(decimal, decimal)> predefinedBalances = new() { (0, 0), (0, 0), (0, 0) };
+
+            //Act
+            var result = transactions.GetBestTrades(Type.Buy, 14.20m, predefinedBalances);
+
+            //Assert
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Transactions_GetBestTrades_BuyPartOfBTC()
+        {
+            //Arrange - variables, classes, mocks
+            string inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input", "test1.json");
+            Transactions transactions = new(inputFilePath);
+            List<(decimal, decimal)> predefinedBalances = new() { (0, 0), (15384, 0), (0, 0) };
+
+            //Act
+            var result = transactions.GetBestTrades(Type.Buy, 14.20m, predefinedBalances);
+            var expectedResult = new List<(string, decimal, decimal)>
+            {
+                ("2024-11-27T15:22:00.2518855Z", 384.00m, 0.80m),
+                ("2024-11-27T15:22:00.2518855Z", 15000.00m, 10.00m)
+            };
+
+            //Assert
+            result.Should().Equal(expectedResult);
+        }
     }
 }
